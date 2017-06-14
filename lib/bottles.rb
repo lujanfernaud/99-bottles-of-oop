c97e69029e2ec6e4c126d439b38c48814f705db5
lujanfernaud <lujanfernaud@gmail.com>
2017-06-14 22:35:24 +0100

    Finish chapter four
    Use the Flocking Rules to discover more abstractions
    and remove the case statement.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,22 +9,19 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, " \
-        "no more bottles of beer.\n" \
-        "Go to the store and buy some more, " \
-        "99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, " \
-        "1 bottle of beer.\n" \
-        "Take it down and pass it around, " \
-        "no more bottles of beer on the wall.\n"
+    "#{quantity(number).capitalize} #{container(number)}" \
+      " of beer on the wall, " \
+      "#{quantity(number)} #{container(number)} of beer.\n" \
+      "#{action(number)}, " \
+      "#{quantity(successor(number))} #{container(successor(number))}" \
+      " of beer on the wall.\n"
+  end
+
+  def quantity(number)
+    if number.zero?
+      "no more"
     else
-      "#{number} bottles of beer on the wall, " \
-        "#{number} bottles of beer.\n" \
-        "Take one down and pass it around, " \
-        "#{number - 1} #{container(number - 1)} of beer on the wall.\n"
+      number.to_s
     end
   end
 
@@ -33,6 +30,30 @@ class Bottles
       "bottle"
     else
       "bottles"
+    end
+  end
+
+  def action(number)
+    if number.zero?
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def successor(number)
+    if number.zero?
+      99
+    else
+      number - 1
     end
   end
 end
