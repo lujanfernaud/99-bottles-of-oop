197bb5363e54377550286d9e018d50a769187e6b
lujanfernaud <lujanfernaud@gmail.com>
2017-06-15 17:17:17 +0100

    Finish chapter six
    Use factory to replace conditionals with polymorphism.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,15 +9,12 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number      = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
 
-    "#{bottle_number.quantity.capitalize} #{bottle_number.container}" \
-      " of beer on the wall, " \
-      "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" \
-      "#{bottle_number.action}, " \
-      "#{next_bottle_number.quantity} #{next_bottle_number.container}" \
-      " of beer on the wall.\n"
+    "#{bottle_number} of beer on the wall, ".capitalize +
+      "#{bottle_number} of beer.\n" +
+      "#{bottle_number.action}, " +
+      "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
@@ -28,43 +25,74 @@ class BottleNumber
     @number = number
   end
 
-  def quantity
-    if number.zero?
-      "no more"
+  def self.for(number)
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    when 6
+      BottleNumber6
     else
-      number.to_s
-    end
+      BottleNumber
+    end.new(number)
+  end
+
+  def to_s
+    "#{quantity} #{container}"
+  end
+
+  def quantity
+    number.to_s
   end
 
   def container
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
+    "bottles"
   end
 
   def action
-    if number.zero?
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun} down and pass it around"
-    end
+    "Take #{pronoun} down and pass it around"
   end
 
   def pronoun
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+    "one"
   end
 
   def successor
-    if number.zero?
-      99
-    else
-      number - 1
-    end
+    BottleNumber.for(number - 1)
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    "no more"
+  end
+
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def successor
+    BottleNumber.for(99)
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
+  end
+end
+
+class BottleNumber6 < BottleNumber
+  def quantity
+    "1"
+  end
+
+  def container
+    "six-pack"
   end
 end
